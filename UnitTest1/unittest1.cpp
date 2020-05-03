de2a04832bb1457d7aa442863c35a55a4b242e92
jiang335 <jiang335@126.com>
2020-05-03 22:13:54 +0800

    splite calcCost code file

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -1,18 +1,49 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include <string>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+extern double calcCostByKilpMetre(int kiloMetre);
+extern double calcCostByMinutes(int minutes);
+extern int calcCost(int kiloMetre, int minutes);
+extern wstring procLineGDK(wstring wstrLine);
 
 namespace UnitTest1
 {		
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(KilpMetre)
 		{
-			// TODO: 在此输入测试代码
+			Assert::AreEqual(calcCostByKilpMetre(-1), 0.0);
+			Assert::AreEqual(calcCostByKilpMetre(0), 0.0);
+			Assert::AreEqual(calcCostByKilpMetre(1), 6.0);
+			Assert::AreEqual(calcCostByKilpMetre(2), 6.0);
+			Assert::AreEqual(calcCostByKilpMetre(4), 7.6);
+			Assert::AreEqual(calcCostByKilpMetre(8), 10.8);
+			Assert::AreEqual(calcCostByKilpMetre(10), 13.2);
 		}
-
+		TEST_METHOD(Minutes)
+		{
+			Assert::AreEqual(calcCostByMinutes(-1), 0.0);
+			Assert::AreEqual(calcCostByMinutes(0), 0.0);
+			Assert::AreEqual(calcCostByMinutes(2), 0.5);
+		}
+		TEST_METHOD(Cost)
+		{
+			Assert::AreEqual(calcCost(1, 0), 6);
+			Assert::AreEqual(calcCost(3, 0), 7);
+			Assert::AreEqual(calcCost(10, 0), 13);
+			Assert::AreEqual(calcCost(2, 3), 7);
+		}
+		TEST_METHOD(Line)
+		{
+			Assert::AreEqual(procLineGDK(L"1公里,等待0分钟").c_str(), L"收费6元");
+			Assert::AreEqual(procLineGDK(L"3公里,等待0分钟").c_str(), L"收费7元");
+			Assert::AreEqual(procLineGDK(L"10公里,等待0分钟").c_str(), L"收费13元");
+			Assert::AreEqual(procLineGDK(L"2公里,等待3分钟").c_str(), L"收费7元");
+		}
 	};
 }
--- tdd-taxi-cpp/tdd-taxi-cpp.cpp
+++ tdd-taxi-cpp/tdd-taxi-cpp.cpp
@@ -4,35 +4,9 @@
 #include <codecvt>
 #include <regex>
 
+#include "calccost.h"
+
 using namespace std;
-
-double calcCostByKilpMetre(int kiloMetre)
-{
-	double result = 0;
-	if (kiloMetre > 0) {
-		result = 6;
-		if (kiloMetre > 2) {
-			result += 0.8 * (kiloMetre - 2);
-			if (kiloMetre > 8)
-				result += 0.8 * 0.5 * (kiloMetre - 8);
-		}
-	}
-	return result;
-}
-
-double calcCostByMinutes(int minutes)
-{
-	double result = 0;
-	if (minutes > 0) {
-		result = 0.25 * minutes;
-	}
-	return result;
-}
-
-int calcCost(int kiloMetre, int minutes)
-{
-	return int(calcCostByKilpMetre(kiloMetre) + calcCostByMinutes(minutes) + 0.5);
-}
 
 wstring procLineGDK(wstring wstrLine)
 {
